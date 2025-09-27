de947a9436e7fc4680352e11ff051ec6aa69806d
AnupongPakee <anuphong4407@gmail.com>
2025-09-27 12:15:34 +0700

    fix: simple key env

--- backend/tests/test_security.py
+++ backend/tests/test_security.py
@@ -1,9 +1,10 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
 from jose import jwt
 from security.token import create_access_token, verify_token
+
+secret_key = os.getenv("SECRET_KEY", "test-secret")
+algorithm = os.getenv("ALGORITHM", "HS256")
 
 def test_create_access_token():
     data = {
@@ -12,8 +13,6 @@ def test_create_access_token():
         'password': '2au8pon4g6'
     }
     token = create_access_token(data=data)
-    secret_key = os.getenv("SECRET_KEY")
-    algorithm = os.getenv("ALGORITHM")
     decoded = jwt.decode(token, secret_key, algorithms=[algorithm])
 
     assert isinstance(token, str)
